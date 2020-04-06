7886fb2e48e8b9fcf7932d95c3e1a74ed25efccb
Christopher Pelatari <chris@pelatari.com>
2020-04-05 19:30:22 -0500

    Update Source.cs

--- Lorem.NET/Source.cs
+++ Lorem.NET/Source.cs
@@ -11,10 +11,8 @@ namespace LoremNET
             return words.Split(" ").OrderBy(x => RandomHelper.Instance.Next());
         }
 
-        internal static IEnumerable<string> WordList(bool includePuncation)
-        {
-            return includePuncation ? Rearrange(Lipsum.Text) : Rearrange(Lipsum.Text.Remove(","));
-        }
+        // https://stackoverflow.com/questions/421616/
+        internal static IEnumerable<string> WordList(bool includePunctuation) => includePunctuation ? Rearrange(Lipsum.Text) : Rearrange(new string(Lipsum.Text.Where(c => !char.IsPunctuation(c)).ToArray()));
 
         public static void Update(string text)
         {
